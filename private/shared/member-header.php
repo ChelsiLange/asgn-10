a6837d0d6950679e1305c058a31ea2e2f2592af0
Chelsi Lange <langechelsi@gmail.com>
2021-11-16 07:32:58 -0500

    begins asgn10

---
+++ private/shared/member-header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>WNC Birds <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="<?= url_for('/stylesheets/public.css'); ?>" />
+  </head>
+
+  <body>
+
+    <header>
+      <h1>
+        <a href="<?= url_for('/index.php'); ?>">
+         
+          WNC Birds
+        </a>
+      </h1>
+    </header>
+
+    <navigation>
+      <ul>
+       <?php
+        if($session->is_logged_in()) { ?>
+            <li>
+                User: <?php echo "$session->username"; ?>
+            </li>
+        <?php } else echo "did not work."; ?>
+      </ul>
+    </navigation>
+  
---
+++ public/members/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once('../../private/initialize.php');
+$page_title = 'Bird: Member Menu'; 
+include(SHARED_PATH . '/member-header.php'); 
+?>
+
+<h2>Main Menu</h2>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
